55d16e58823e6b4ae1159cf899e1dce7f3eba3b7
Can Vardar <ismailcanvardar@gmail.com>
2023-10-09 19:34:58 +0300

    add setBalance method to helpers
    Co-Authored-By: Mehmet Rauf Oğuz <88249309+mehmetraufoguz@users.noreply.github.com>

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -2,11 +2,12 @@ import { BigNumber, ethers } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { HardhatEthersHelpers } from "@nomiclabs/hardhat-ethers/types";
 import { cos, unit } from "mathjs";
+import { Network } from "hardhat/types";
 
 const PI = "3141592653589793238";
 
 type Entry<T> = {
-  [K in keyof T]: [K, T[K]]
+  [K in keyof T]: [K, T[K]];
 }[keyof T];
 
 function filterObject<T extends object>(
@@ -56,30 +57,61 @@ const calculateExchangeFee = (amount: number, percentage: number) => {
   return (amount * percentage) / 100;
 };
 
-const calculateClaimableAmount = (blockTimestamp: BigNumber, lastClaimTimestamp: BigNumber, periodLength: number, claimableAmount: BigNumber, distributionRate: number) => {
+const calculateClaimableAmount = (
+  blockTimestamp: BigNumber,
+  lastClaimTimestamp: BigNumber,
+  periodLength: number,
+  claimableAmount: BigNumber,
+  distributionRate: number
+) => {
   const decimals = BigNumber.from(10).pow(18);
-  return (claimableAmount.mul(distributionRate).mul(blockTimestamp.sub(lastClaimTimestamp)).mul(decimals)).div(BigNumber.from(periodLength).mul(10_000).mul(decimals));
+  return claimableAmount
+    .mul(distributionRate)
+    .mul(blockTimestamp.sub(lastClaimTimestamp))
+    .mul(decimals)
+    .div(BigNumber.from(periodLength).mul(10_000).mul(decimals));
 };
 
-const calculateBurnAmount = (currentPrice: BigNumber, blocksInTwoMonths: number, constantValueFromFormula: number, totalBurnedAmount: number) => {
+const calculateBurnAmount = (
+  currentPrice: BigNumber,
+  blocksInTwoMonths: number,
+  constantValueFromFormula: number,
+  totalBurnedAmount: number
+) => {
   const decimals = BigNumber.from(10).pow(18);
-  const a = BigNumber.from(blocksInTwoMonths).mul(BigNumber.from(13).mul(BigNumber.from(10).pow(4))).mul(decimals);
-  const b = (BigNumber.from(100).mul(currentPrice)).add(BigNumber.from(constantValueFromFormula).mul(decimals));
-  const c = (a.div(b)).mul(decimals);
-  const radiant = (((BigNumber.from(totalBurnedAmount).div(BigNumber.from(10).pow(9))).add(BigNumber.from(86).mul(decimals))).mul(BigNumber.from(PI))).div(BigNumber.from(180).mul(decimals));
+  const a = BigNumber.from(blocksInTwoMonths)
+    .mul(BigNumber.from(13).mul(BigNumber.from(10).pow(4)))
+    .mul(decimals);
+  const b = BigNumber.from(100)
+    .mul(currentPrice)
+    .add(BigNumber.from(constantValueFromFormula).mul(decimals));
+  const c = a.div(b).mul(decimals);
+  const radiant = BigNumber.from(totalBurnedAmount)
+    .div(BigNumber.from(10).pow(9))
+    .add(BigNumber.from(86).mul(decimals))
+    .mul(BigNumber.from(PI))
+    .div(BigNumber.from(180).mul(decimals));
   const cosine = toWei(String(cos(Number(ethers.utils.formatEther(radiant)))));
-  const d = (cosine.mul(BigNumber.from(2923))).div(BigNumber.from(10).pow(3));
+  const d = cosine.mul(BigNumber.from(2923)).div(BigNumber.from(10).pow(3));
 
-  return (c.mul(d)).div(decimals);
+  return c.mul(d).div(decimals);
 };
 
 const findMarginOfDeviation = (V1: BigNumber, V2: BigNumber) => {
   let absDiff: string | BigNumber | number = V1.sub(V2).abs();
-  let absAverage: string | BigNumber | number = (V1.add(V2)).div(2).abs();
+  let absAverage: string | BigNumber | number = V1.add(V2).div(2).abs();
   absDiff = ethers.utils.formatUnits(absDiff);
   absAverage = ethers.utils.formatUnits(absAverage);
 
   return (+absDiff / +absAverage) * 100;
+};
+
+const setBalance = async (
+  network: Network,
+  address: string,
+  amountInHex: string
+) => {
+  await network.provider.send("hardhat_setBalance", [address, amountInHex]);
 };
 
 export {
@@ -91,5 +123,6 @@ export {
   filterObject,
   calculateClaimableAmount,
   calculateBurnAmount,
-  findMarginOfDeviation
+  findMarginOfDeviation,
+  setBalance,
 };
