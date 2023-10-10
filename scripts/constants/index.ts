1e972e79e84c4eaa667d6fbe55fb42abc37ab55c
Can Vardar <ismailcanvardar@gmail.com>
2023-10-10 18:26:57 +0300

    remove POOL_PARAMS & CONSTRUCTOR_PARAMS

--- scripts/constants/index.ts
+++ scripts/constants/index.ts
@@ -1,5 +1,4 @@
 import CHAIN_IDS from "./chain-ids";
-import CONSTRUCTOR_PARAMS from "./constructor-params";
 import CONTRACTS from "./contracts";
 
-export { CHAIN_IDS, CONSTRUCTOR_PARAMS, CONTRACTS };
+export { CHAIN_IDS, CONTRACTS };
